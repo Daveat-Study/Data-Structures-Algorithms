2dddc855d7b7c8992a39d82298bf6f734f426968
DaveatCor <condaveat@gmail.com>
2024-05-09 22:16:57 +0700

    feat(DSA): initialize course

---
+++ test/data_structures_algorithms_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_structures_algorithms_in_dart/data_structures_algorithms_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
